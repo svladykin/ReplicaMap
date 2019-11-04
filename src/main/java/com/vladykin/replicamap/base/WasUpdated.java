d145803c9f5ab30e78f0dc5f206685d79dddf3ff
Sergi Vladykin <sergi.vladykin@gmail.com>
2019-11-04 07:41:36 +0300

    javadoc

--- src/main/java/com/vladykin/replicamap/base/WasUpdated.java
+++ src/main/java/com/vladykin/replicamap/base/WasUpdated.java
@@ -1,14 +1,18 @@
 package com.vladykin.replicamap.base;
 
+import com.vladykin.replicamap.ReplicaMapListener;
 import java.util.function.BiFunction;
 
 /**
  * {@link ReplicaMapBase} itself can not always reliably detect if the function
  * has modified the value or not. This may happen when you modify the value inplace
- * instead of creating a modified copy. If the function implements this interface
- * {@link ReplicaMapBase} can ask the update status of the last function invocation
- * using the method {@link #wasUpdated()}, otherwise {@link ReplicaMapBase} will
- * just check the old and the new values for equality.
+ * instead of creating a modified copy. In this case {@link ReplicaMapListener}
+ * will not be invoked.
+ *
+ * If the function implements this interface {@link ReplicaMapBase} can ask the
+ * update status of the last invocation using the method {@link #wasUpdated()},
+ * otherwise {@link ReplicaMapBase} will just check the old and the new values for equality
+ * to decide if we need to fire {@link ReplicaMapListener}.
  *
  * @see ReplicaMapBase#compute(Object, BiFunction)
  * @see ReplicaMapBase#computeIfPresent(Object, BiFunction)
@@ -18,7 +22,7 @@ import java.util.function.BiFunction;
  */
 public interface WasUpdated {
     /**
-     * @return {@code true}
+     * @return {@code true} If the last execution of this function updated the value.
      */
     boolean wasUpdated();
 }
